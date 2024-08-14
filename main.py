bfa2f444d5c5303e2ffbb92f7494f230f3e389dc
Julian <julian.costinea@gmail.com>
2024-08-14 18:19:58 +0200

    fixed using selenium. added mail func

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-import requests
-import lxml
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 import csv
+import time
+import smtplib
+from email.message import EmailMessage
+import os
+from dotenv import load_dotenv
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-    "Accept-Language": "en-US,en;q=0.5"
-}
+load_dotenv()
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+browser = webdriver.Chrome(options=chrome_options)
+gmail_password = os.environ.get("GMAIL_PASSWORD")
 
 product_links = []
 product_prices = []
+
+# email setup
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.starttls()
+# Authentication
+s.login("julian.costinea@gmail.com", gmail_password)
+# Create a MIME text object
+msg = EmailMessage()
+msg['From'] = "julian.costinea@gmail.com"
+msg['To'] = "emil.costinea@gmail.com"
+msg['Subject'] = 'Consider buying this product'
 
 with open("products.csv", encoding='utf-8-sig') as file:
     contents = csv.reader(file)
@@ -18,11 +36,9 @@ with open("products.csv", encoding='utf-8-sig') as file:
         product_prices.append(row[1])
 
 for i in range(len(product_links)):
-    response = requests.get(product_links[i], headers=headers)
-    soup = BeautifulSoup(response.content, "lxml")
-    pretty_soup = soup.prettify()
-    print(pretty_soup)
-    price_span = soup.find(name="span", class_="a-price-whole")
-    price = int(price_span.getText().split('.')[0])
+    browser.get(product_links[i])
+    time.sleep(5)
+    price = int(browser.find_element(By.CLASS_NAME, "a-price-whole").text)
     if price < int(product_prices[i]):
-        print(f"Might wanna buy the product: {product_links[i]}")
+        msg.set_content(f"Product link: {product_links[i]}")
+        s.send_message(msg)
